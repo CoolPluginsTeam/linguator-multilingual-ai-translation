8f0ceebbd2ec4c112c4e362ba8b527d175d0e577
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2025-08-29 18:28:16 +0530

    integrate langauge switcher elementor widget

--- integrations/elementor/load.php
+++ integrations/elementor/load.php
@@ -9,5 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Load Elementor compatibility
 if ( lmat_is_plugin_active( 'elementor/elementor.php' ) ) {
 	require_once __DIR__ . '/elementor.php';
+	require_once __DIR__ . '/lmat-register-widget.php';
 	new Linguator\Integrations\elementor\LMAT_Elementor();
+	new Linguator\Integrations\elementor\LMAT_Register_Widget();
 }
