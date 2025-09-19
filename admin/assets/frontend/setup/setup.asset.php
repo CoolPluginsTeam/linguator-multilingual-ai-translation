51b9930b0a4163e1eef093ce763e7fd57cef57a3
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2025-09-19 18:29:54 +0530

    minor textual changes

--- admin/assets/frontend/setup/setup.asset.php
+++ admin/assets/frontend/setup/setup.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '19dedca108aaed34b6d1');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '1bde40d16b1ecb42e252');
