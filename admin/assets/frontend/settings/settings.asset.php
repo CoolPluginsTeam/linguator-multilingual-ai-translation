70626321f22a170fda77f08fae4cc7269ed3c388
Abishek Newar <abisheknewarcoolplugins@gmail.com>
2025-09-20 12:53:31 +0530

    icon changes

--- admin/assets/frontend/settings/settings.asset.php
+++ admin/assets/frontend/settings/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'c70b8f0edfef8bde885e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'cdd405d991a0d001254a');
--- admin/assets/frontend/setup/setup.asset.php
+++ admin/assets/frontend/setup/setup.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '19dedca108aaed34b6d1');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '8647d8996448f057fb7c');
