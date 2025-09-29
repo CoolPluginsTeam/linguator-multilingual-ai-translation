b3c4b4897625b5a893864e847f017bf6252927e8
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2025-09-29 18:32:27 +0530

    integrate language list button in elementor editor

--- admin/assets/classic-inline-translate/index.asset.php
+++ admin/assets/classic-inline-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5a401b99dce3b6689063');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bd434d293e0bb4ab2b4d');
