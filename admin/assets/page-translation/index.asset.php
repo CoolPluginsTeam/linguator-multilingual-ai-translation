0e66f97bfcf163af9329cf12aed3bbe9f479a738
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-09-30 16:35:06 +0530

    Fixed google translate dropdown issue in page translation.
    Fixed google translate dropdown issue in page translation.
    Improve close button styling in translation popup modal.

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '175de80cc5a692fd3c94');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b777b68cfa40fd2cd22f');
