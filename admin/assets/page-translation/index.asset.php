e508d4b6cb3a2a40f9d1d60c311efc24d51866d7
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-08 13:07:50 +0530

    Fixed content format issue in table content.
    Fixed content format issue in table content.

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3f9797577637830412bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '17bb0cfca301bc5b47ff');
