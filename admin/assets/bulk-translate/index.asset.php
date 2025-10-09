31e05ef08e43d60498c3e1a924bbfb60e1695808
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-09 17:29:52 +0530

    Fixed core table block and inner html content translation issue in bulk translation.
    Fixed core table block and inner html content translation issue in bulk translation.

--- admin/assets/bulk-translate/index.asset.php
+++ admin/assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '38f365162ad7ccc3643e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '8683cea5f0a9271647b1');
