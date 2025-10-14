1d7dcd6cda72b6a95796bede2f26a8d82c80f961
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2025-10-14 17:26:07 +0530

    fix langauge search issue

--- admin/assets/frontend/setup/setup.asset.php
+++ admin/assets/frontend/setup/setup.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'b68b67dd39cc39d5c617');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '0218ca1fb317d50c30f1');
