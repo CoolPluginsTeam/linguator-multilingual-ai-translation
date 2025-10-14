56d955aae2df6ae4e6fb21ac4231059a9eb8e33a
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-14 12:21:09 +0530

    Set minimize true in webpack.config.js.
    1. Set minimize true in webpack.config.js to change mode development to production mode and compress file size.

--- admin/assets/frontend/setup/setup.asset.php
+++ admin/assets/frontend/setup/setup.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '21da6f29e3289eeab1a7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '6f0d39c7dbd9fe55cfbe');
