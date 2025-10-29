40dc4e3dbab932d071da0c598da51e7516df3f3d
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-29 16:25:34 +0530

    Fixed string modal open If language not supported.
    1. Fixed string modal open If language not supported.

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '52719460e22b727facba');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '85cee027cf9c69d9ad4a');
