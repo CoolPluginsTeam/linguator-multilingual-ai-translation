060b7e4c388e853327674d37ab1ccab2c1b8330c
sushil <sushilcoolplugins@gmail.com>
2025-10-31 17:19:53 +0530

    minor
    UI change

--- admin/assets/frontend/settings/settings.asset.php
+++ admin/assets/frontend/settings/settings.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '756ec5e4c12f40396e06');
+=======
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'd1d032a275c0b114561d');
+>>>>>>> Stashed changes
