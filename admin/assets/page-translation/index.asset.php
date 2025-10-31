85c045cda53a316b74f937c9cd264d3f9740a851
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-31 11:59:26 +0530

    Fixed wysiwyg content line break content formatting issue.
    Fixed wysiwyg content line break content formatting issue.

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '85cee027cf9c69d9ad4a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '773f3252b0d026048a9a');
