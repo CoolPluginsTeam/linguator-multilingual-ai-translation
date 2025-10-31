aa9c3bf42bb5aaf2ab72a5155f4553275631db01
sushil <sushilcoolplugins@gmail.com>
2025-10-31 17:35:19 +0530

    update build
    UI change

--- admin/assets/frontend/settings/settings.asset.php
+++ admin/assets/frontend/settings/settings.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '756ec5e4c12f40396e06');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'd1d032a275c0b114561d');
->>>>>>> Stashed changes
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'e872bea73418e1adba26');
