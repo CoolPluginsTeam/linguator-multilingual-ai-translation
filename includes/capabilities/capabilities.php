f4d57f5da6c90dfc5b9efcb41787f1edb76ef3f4
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-11-14 12:38:41 +0530

    Added custom capabilities for manage languages.
    Added custom capabilities for manage languages.

---
+++ includes/capabilities/capabilities.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package Linguator
+ */
+
+namespace Linguator\Includes\Capabilities;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * A class allowing to map Linguator's custom user capabilities to WP's native ones.
+ *
+ * @since 3.8
+ */
+class Capabilities {
+	public const LANGUAGES    = 'manage_languages';
+	public const TRANSLATIONS = 'manage_translations';
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.8
+	 */
+	public function __construct() {
+		add_filter( 'map_meta_cap', array( $this, 'map_custom_caps' ), 1, 2 );
+	}
+
+	/**
+	 * Filters user capabilities to handle LMAT's custom capabilities.
+	 *
+	 * @since 3.8
+	 *
+	 * @param string[] $caps Primitive capabilities required by the user.
+	 * @param string   $cap  Capability being checked.
+	 * @return string[]
+	 */
+	public function map_custom_caps( $caps, $cap ) {
+		if ( in_array( $cap, array( self::TRANSLATIONS, self::LANGUAGES ), true ) ) {
+			$caps   = array_diff( $caps, array( $cap ) );
+			$caps[] = 'manage_options';
+		}
+
+		return $caps;
+	}
+}
