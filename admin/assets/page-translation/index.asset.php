57025d9e8a44e69caa85f538c81dc4b00749a6f1
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-11-28 10:48:21 +0530

    Fixed glossary UI styling issues.
    Fixed glossary UI styling issues.

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b84932715c7bd92c536b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '675091fb9db8a493bab7');
