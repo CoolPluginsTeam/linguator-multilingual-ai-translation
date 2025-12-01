3dbf8b1a080af70feab1a90dd8d7907a451fd5eb
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-12-01 12:09:26 +0530

    Fixed styling conflict issue with browser extension.
    Fixed single page translation table column styling conflict issue with browser extension.

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7dfca5af3ff8f9fc8141');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '20676a522eabc6182d98');
