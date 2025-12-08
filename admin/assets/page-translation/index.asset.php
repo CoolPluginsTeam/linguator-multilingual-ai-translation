e341f65bc02165ea90270dfd5ac474e10b44ced6
sushil <sushilcoolplugins@gmail.com>
2025-12-08 17:20:49 +0530

    texual changes

--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '33ffcccc93485cdcaa61');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9ae55a802c4da11e001f');
