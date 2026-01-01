7c4f866a10cbe18261661b3dc1f5b92e8cf1652f
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2026-01-01 14:52:58 +0530

    fix minor issues

--- admin/supported-blocks/supported-blocks.php
+++ admin/supported-blocks/supported-blocks.php
@@ -134,8 +134,8 @@ if ( ! class_exists( 'Supported_Blocks' ) ) {
 			?>
 		<div class="lmat-custom-data-table-wrapper">
 			<h3><?php echo esc_html__('Supported Blocks Translation Settings', 'linguator-multilingual-ai-translation'); ?>
-			<br>
-			<?php 
+			<br><p>
+			<?php
 				// translators: %s: Linguator.
 				printf( esc_html__( 'Manage Gutenberg blocks to make them translation-ready with %s.', 'linguator-multilingual-ai-translation' ), 'Linguator' ); 
 			?></p>
