10798b1be4cd3b2087038b743772442763546cb4
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2026-03-21 14:52:50 +0530

    update readme and version (1.1.0)

--- linguator-multilingual-ai-translation.php
+++ linguator-multilingual-ai-translation.php
@@ -3,7 +3,7 @@
  * Plugin Name:      Multilingual AI Translator
  * Plugin URI:        https://linguator.com/
  * Description:       Create a multilingual WordPress website in minutes withMultilingual AI Translator.
- * Version:           1.0.4
+ * Version:           1.1.0
  * Requires at least: 6.2
  * Requires PHP:      7.2
  * Author:            Cool Plugins
@@ -37,7 +37,7 @@ use Linguator\Install\Linguator_Usable;
 
 // Linguator constants - wrapped in checks to prevent redeclaration
 if ( ! defined( 'LINGUATOR_VERSION' ) ) {
-	define( 'LINGUATOR_VERSION', '1.0.4' );
+	define( 'LINGUATOR_VERSION', '1.1.0' );
 }
 if ( ! defined( 'LMAT_MIN_WP_VERSION' ) ) {
 	define( 'LMAT_MIN_WP_VERSION', '6.2' );
