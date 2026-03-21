7a054dbdafcfeedba6b4b6630e6d7176ee6388e1
VishaliCoolPlugins <vishalicoolplugins@gmail.com>
2026-03-21 15:04:56 +0530

    textual changes

--- linguator-multilingual-ai-translation.php
+++ linguator-multilingual-ai-translation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:      Multilingual AI Translator
  * Plugin URI:        https://linguator.com/
- * Description:       Create a multilingual WordPress website in minutes withMultilingual AI Translator.
+ * Description:       Create a multilingual WordPress website in minutes with Multilingual AI Translator.
  * Version:           1.1.0
  * Requires at least: 6.2
  * Requires PHP:      7.2
