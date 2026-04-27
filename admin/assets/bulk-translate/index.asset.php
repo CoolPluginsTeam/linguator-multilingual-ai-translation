58345f61280582cf3a9713f5cc63696c85aac419
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-04-27 11:34:06 +0530

    Improve content filtration in single & bulk page translation.
    Improve content filtration in single & bulk page translation.

--- admin/assets/bulk-translate/index.asset.php
+++ admin/assets/bulk-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c7e4fd9e2749b6121320');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'ee2923d4cae94993659a');
--- admin/assets/page-translation/index.asset.php
+++ admin/assets/page-translation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '44b1d9ca776e0f6ff480');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '88b3e5c1d3245ddabb31');
